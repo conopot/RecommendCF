b4e56e12bf74900266274f62fbc4a8022c220991
Taehyun <kimtaehyun98@naver.com>
2022-09-18 14:39:46 +0900

    feat: get data from cloudfront

--- main.py
+++ main.py
@@ -11,6 +11,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
+def getFileFromCloudFront(cloudFrontUrl, filePath):
+  file = requests.get(cloudFrontUrl + filePath, allow_redirects=True)
+  url_content = file.content
+  csv_file = open(filePath,'wb')
+  csv_file.write(url_content)
+  csv_file.close()
+
 # test code
 @app.route("/")
 def hello_world():
@@ -19,12 +26,26 @@ def hello_world():
 
 @app.route("/recommendCF")
 def recommend():
-    
-    # test env path
-    cloudFrontUrl = os.environ.get("CLOUD_FRONT_URL")
-    print(cloudFrontUrl)
+    # Get data from CloudFront
 
-    return "CloudFrontUrl : {}".format(cloudFrontUrl)
+    # Get CloudFront url from env
+    cloudFrontUrl = os.environ.get("CLOUD_FRONT_URL")
+
+    # Get rating data
+    ratingsPath = "ratings.csv"
+    getFileFromCloudFront(cloudFrontUrl, ratingsPath)
+    rating_data = pd.read_csv(ratingsPath)
+
+    # Get music data
+    musicsPath = "musics.csv"
+    getFileFromCloudFront(cloudFrontUrl, musicsPath)
+    music_data = pd.read_csv(musicsPath)
+
+    # Test code
+    print(rating_data.head(2))
+    print(music_data.head(2))
+
+    return "Rating data size {}".format(str(rating_data.size))
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
